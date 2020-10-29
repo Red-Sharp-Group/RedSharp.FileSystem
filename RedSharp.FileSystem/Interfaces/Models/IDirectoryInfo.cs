6c811010c6fafb7b35862e80c7f37c36ec6f2d6b
Kudriavtsev Andrii <extosis.vt@gmail.com>
2020-10-29 22:14:22 +0200

    Basic models were added to the projects. Path model and processing it - in progress.

---
+++ RedSharp.FileSystem/Interfaces/Models/IDirectoryInfo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedSharp.FileSystem.Interfaces.Models
+{
+    public interface IDirectoryInfo : IDriveItem
+    { }
+}
